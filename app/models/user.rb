fd587a34b668026fdebed71ed069040daea1f04c
Marcelo Silva <marcelo.cansilva@gmail.com>
2022-09-18 15:00:16 -0300

    Added letter opener

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :welcome_user
+
+  private
+
+  def welcome_user
+    UserMailer.with(user: self).welcome.deliver_now
+  end
 end
